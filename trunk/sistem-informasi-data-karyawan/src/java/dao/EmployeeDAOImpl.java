178f9b47772aa5a6c68584f16acb37f26f981127
geladak.begundal@gmail.com <geladak.begundal@gmail.com@f83a3bb8-c897-1a4f-626f-d78d71d8db95>
2012-07-11 21:09:47 +0000

    Tampil Data employee

--- trunk/sistem-informasi-data-karyawan/src/java/dao/EmployeeDAOImpl.java
+++ trunk/sistem-informasi-data-karyawan/src/java/dao/EmployeeDAOImpl.java
@@ -45,4 +45,17 @@ public class EmployeeDAOImpl extends GeneralDAOImpl
         }
         return list;
     }
+
+    @Override
+    public List<Employee> gets(String id) throws Exception {
+        List<Employee> list = new ArrayList<Employee>();
+        try {
+            this.em.getTransaction().begin();
+            list = this.em.createQuery("SELECT o FROM Employee o WHERE o.id LIKE :'%id%'").setParameter("id", id).getResultList();
+            this.em.getTransaction().commit();
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return list;
+    }
 }
